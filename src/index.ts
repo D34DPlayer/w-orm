aef9d8722befca138bc48b211c9ccffb2a9bea2a
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-03-27 10:37:08 +0200

    (chore) add example for testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export { }
+import { deleteDB, init } from './connection.js'
+import { Model } from './models.js'
+import { Field } from './fields.js'
+
+class ExampleModel extends Model {
+  @Field({ primaryKey: true })
+  name!: string
+
+  @Field({ primaryKey: false, default: true })
+  test!: boolean
+
+  @Field({ primaryKey: false, default: 0 })
+  amount!: number
+}
+
+async function main() {
+  await deleteDB('test').catch(console.error)
+  const db = await init('test', 1).catch(console.error)
+}
+
+main().catch(console.error)
