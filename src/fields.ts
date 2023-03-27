f87b78009a7278e7dc3885c8e554bfaebf67ec3c
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-03-27 10:36:09 +0200

    (feat) Field decorator and metadata

---
+++ src/fields.ts
@@ -0,0 +1,26 @@
+import { addFieldToMetadata } from './metadata.js'
+
+export interface FieldOptions<T> {
+  primaryKey: boolean
+  unique: boolean
+  default?: T | (() => T)
+  type: () => T
+}
+
+export function Field<T>(options: Partial<FieldOptions<T>> = {}): PropertyDecorator {
+  return function (object, propertyName) {
+    if (typeof propertyName !== 'string')
+      throw new Error('Field decorator can only be used on fields')
+
+    const type = Reflect.getMetadata('design:type', object, propertyName) as () => T
+
+    const newOptions: FieldOptions<T> = {
+      primaryKey: false,
+      unique: false,
+      type,
+      ...options,
+    }
+
+    addFieldToMetadata<T>(object.constructor.name, propertyName, newOptions)
+  }
+}
---
+++ src/metadata.ts
@@ -0,0 +1,42 @@
+import type { FieldOptions } from './fields.js'
+import { db } from './connection.js'
+
+interface TableMetadata {
+  [fieldName: string]: FieldOptions<any>
+}
+
+export const TablesMetadata: Record<string, TableMetadata> = {}
+
+export function addFieldToMetadata<T>(tableName: string, fieldName: string, options: FieldOptions<T>) {
+  if (!TablesMetadata[tableName])
+    TablesMetadata[tableName] = {}
+  TablesMetadata[tableName][fieldName] = options
+}
+
+function getPrimaryKeys(tableName: string) {
+  const table = TablesMetadata[tableName]
+  if (!table)
+    return []
+
+  const primaryKeys = []
+  for (const fieldName in table) {
+    const field = table[fieldName]
+    if (field.primaryKey)
+      primaryKeys.push(fieldName)
+  }
+  return primaryKeys
+}
+
+export function createTables() {
+  if (!db.connected)
+    throw new Error('Database is not connected')
+  for (const tableName in TablesMetadata) {
+    const table = TablesMetadata[tableName]
+    const primaryKeys = getPrimaryKeys(tableName)
+    const store = db.session.createObjectStore(tableName, { keyPath: primaryKeys })
+    for (const fieldName in table) {
+      const field = table[fieldName]
+      store.createIndex(fieldName, fieldName, { unique: field.unique })
+    }
+  }
+}
