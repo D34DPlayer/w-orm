7d97fa37c9aa0d3909047b81128c72171f7971c1
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-04-01 02:08:10 +0200

    (test) define connection.ts spec

--- test/connection.spec.ts
+++ test/connection.spec.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai'
 
-import { db, disconnect, init } from '../src/connection'
+import { db, deleteDB, disconnect, init } from '../src/connection'
 
 describe('DB Tests', () => {
   it('should be disconnected by default', () => {
@@ -9,6 +9,18 @@ describe('DB Tests', () => {
   it('should connect to the database', async () => {
     await init('test', 1)
     assert(db.connected)
+  })
+  it('should disconnect from the database', async () => {
+    await init('test', 1)
+    disconnect()
+    assert(!db.connected)
+  })
+  it('should\'t connect to the database twice', async () => {
+    await init('test', 1)
+    await init('test', 1)
+    assert(db.connected)
+
+    disconnect()
   })
 })
 
@@ -27,5 +39,20 @@ describe('Migrations', () => {
 
     const initResp = await init('test', 1)
     assert(!initResp.upgraded)
+  })
+})
+
+describe('Delete DB', () => {
+  it('should delete the database', async () => {
+    await init('test', 1)
+    disconnect()
+    await deleteDB('test')
+    const databases = await globalThis.indexedDB.databases()
+    assert.lengthOf(databases, 0)
+  })
+  it('shouldn\'t delete the database if it doesn\'t exist', async () => {
+    await deleteDB('test')
+    const databases = await globalThis.indexedDB.databases()
+    assert.lengthOf(databases, 0)
   })
 })
