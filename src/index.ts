4ee4a4d0051989baadaa543e63572e8c0a1e7a3e
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-04-05 22:41:31 +0200

    (feat) Export transaction module

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 
 export * from './connection'
 export * from './fields'
-export * from './models'
 export * from './metadata'
+export * from './models'
 export * from './query'
+export * from './transaction'
