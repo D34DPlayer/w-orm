098b85307d01dbd1970241bd09687a7f29088acc
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-04-22 01:06:23 +0200

    (hotfix) expose migrations at index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
 export * from './connection'
 export * from './fields'
 export * from './metadata'
+export * from './migration'
 export * from './models'
 export * from './query'
 export * from './transaction'
