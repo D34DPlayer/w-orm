461077ec8ec24e29e5e0febcaedf0870a371b94e
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-04-29 00:58:11 +0200

    (feat) Make Transactions return something

--- src/types.ts
+++ src/types.ts
@@ -64,7 +64,7 @@ export interface SimpleFilter<T extends Model> {
 
 export type OrderBy<T extends Model> = (ModelFieldKey<T>) | `-${ModelFieldKey<T>}`
 
-export type TransactionCallback = (tx: IDBTransaction) => Promise<void>
+export type TransactionCallback = (tx: IDBTransaction) => Promise<unknown>
 
 export type TransactionOrMode = IDBTransaction | IDBTransactionMode
 
