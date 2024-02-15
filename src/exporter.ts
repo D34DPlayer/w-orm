d99dcc2701442c3f1543f916e887a098c9f054fc
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2024-02-15 23:53:09 +0100

    (feat) export/import functionality

---
+++ src/exporter.ts
@@ -0,0 +1,108 @@
+import { db } from './connection'
+import { ConnectionError } from './errors'
+
+/*
+* Export a table from the database
+* This is equivalent to calling Table.all() but without wrapping the result in the table class.
+*
+* @param table - The name of the table to export
+* @returns An array containing the data from the table
+*/
+export async function exportTable(table: string): Promise<unknown[]> {
+  if (!db.connected)
+    throw new ConnectionError('Database not connected')
+
+  const transaction = db.session.transaction(table, 'readonly')
+
+  const store = transaction.objectStore(table)
+  const request = store.getAll()
+
+  return new Promise((resolve, reject) => {
+    request.onerror = (event) => {
+      reject(event)
+    }
+
+    request.onsuccess = () => {
+      resolve(request.result)
+    }
+  })
+}
+
+/*
+* Import data into a table
+* @param table - The name of the table to import into
+* @param entries - The data to import
+* @param tx - An optional transaction to use
+* @returns The number of entries imported
+*/
+export async function importTable(table: string, entries: unknown[], tx?: IDBTransaction): Promise<number> {
+  if (!db.connected)
+    throw new ConnectionError('Database not connected')
+
+  if (!tx)
+    tx = db.session.transaction(table, 'readwrite')
+
+  const store = tx.objectStore(table)
+  const promises = []
+  for (const entry of entries) {
+    promises.push(new Promise<boolean>((resolve, reject) => {
+      const request = store.put(entry)
+      request.onerror = (event) => {
+        reject(event)
+      }
+      request.onsuccess = () => {
+        resolve(true)
+      }
+    }))
+  }
+
+  return Promise.all(promises).then(x => x.length)
+}
+
+/*
+* Export the entire database.
+* @returns An object containing the data from each table in the database
+*/
+export async function exportDatabase(blacklist: string[] = []): Promise<Record<string, unknown[]>> {
+  if (!db.connected)
+    throw new ConnectionError('Database not connected')
+
+  const exportData: Record<string, unknown[]> = {}
+  const tables = Array.from(db.session.objectStoreNames).filter(table => !blacklist.includes(table))
+
+  for (const table of tables)
+    exportData[table] = await exportTable(table)
+
+  return exportData
+}
+
+export async function importDatabase(data: Record<string, unknown[]>): Promise<Record<string, number>> {
+  if (!db.connected)
+    throw new ConnectionError('Database not connected')
+
+  const tx = db.session.transaction(Array.from(db.session.objectStoreNames), 'readwrite')
+
+  const promises = []
+  for (const table in data)
+    promises.push(importTable(table, data[table], tx).then(n => [table, n]))
+
+  return Promise.all(promises).then(results => Object.fromEntries(results) as Record<string, number>)
+}
+
+/*
+* Convert an object to a Blob
+*
+* @param object - The object to convert
+* @returns A Blob containing the JSON representation of the object
+*/
+function objectToBlob(object: unknown): Blob {
+  const str = JSON.stringify(object, null, 2)
+  return new Blob([str], { type: 'application/json' })
+}
+
+/*
+* Export the entire database to a Blob
+*/
+export function exportDatabaseToBlob(): Promise<Blob> {
+  return exportDatabase().then(objectToBlob)
+}
---
+++ test/exporter.spec.ts
@@ -0,0 +1,155 @@
+import { assert, describe, it } from 'vitest'
+
+import { Model } from '../src/models'
+import { init } from '../src/connection'
+import { Field } from '../src/fields'
+import { Transaction } from '../src/transaction'
+import { exportDatabase, exportDatabaseToBlob, exportTable, importDatabase, importTable } from '../src/exporter'
+import { ConnectionError } from '../src/errors'
+
+describe('DB Exporter', () => {
+  it('fails when no connection has been established', async () => {
+    class Test extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+    }
+
+    await exportTable(Test.name).then(() => assert.fail('Should have failed')).catch((e) => {
+      assert.isTrue(e instanceof ConnectionError)
+    })
+    await exportDatabase().then(() => assert.fail('Should have failed')).catch((e) => {
+      assert.isTrue(e instanceof ConnectionError)
+    })
+    await importTable(Test.name, []).then(() => assert.fail('Should have failed')).catch((e) => {
+      assert.isTrue(e instanceof ConnectionError)
+    })
+    await importDatabase({ Test: [] }).then(() => assert.fail('Should have failed')).catch((e) => {
+      assert.isTrue(e instanceof ConnectionError)
+    })
+  })
+  it('should be able to export a single table', async () => {
+    class Test extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+
+      tags: string[] = []
+    }
+
+    await init('test', 1)
+
+    const test = await Test.create({ id: 1, tags: ['a', 'b'] })
+    const test2 = await Test.create({ id: 2, tags: ['c', 'd'] })
+
+    const exported = await exportTable(Test.name)
+    assert.deepEqual(exported, [test, test2])
+  })
+
+  it('should be able to export multiple tables', async () => {
+    class Test extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+
+      tags: string[] = []
+    }
+
+    class Test2 extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+
+      username!: string
+    }
+
+    await init('test', 1)
+
+    const test = await Test.create({ id: 1, tags: ['a', 'b'] })
+    const test2 = await Test.create({ id: 2, tags: ['c', 'd'] })
+    const test3 = await Test2.create({ id: 1, username: 'test' })
+    const test4 = await Test2.create({ id: 2, username: 'test2' })
+
+    const exported = await exportDatabase()
+    assert.deepEqual(exported, {
+      Test: [test, test2],
+      Test2: [test3, test4],
+    })
+  })
+
+  it('should be able to export to a JSON Blob', async () => {
+    class Test extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+
+      random!: number
+    }
+
+    await init('test', 1)
+    await Transaction('readwrite', [Test], async (tx) => {
+      const promises: Promise<Test>[] = []
+      for (let i = 0; i < 10000; i++)
+        promises.push(Test.create({ id: i, random: Math.random() }, tx))
+
+      await Promise.all(promises)
+    })
+
+    const exported = await exportDatabaseToBlob()
+    assert.isTrue(exported instanceof Blob)
+  })
+
+  it('should be able to import a single table', async () => {
+    class Test extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+
+      tags: string[] = []
+    }
+
+    await init('test', 1)
+
+    const data = [
+      { id: 1, tags: ['a', 'b'] },
+      { id: 2, tags: ['c', 'd'] },
+    ]
+
+    await importTable(Test.name, data)
+
+    const tests = await Test.all()
+
+    assert.deepEqual(tests, data)
+  })
+
+  it('should be able to import multiple tables', async () => {
+    class Test extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+
+      tags: string[] = []
+    }
+
+    class Test2 extends Model {
+      @Field({ primaryKey: true })
+      id!: number
+
+      username!: string
+    }
+
+    await init('test', 1)
+
+    const data = {
+      Test: [
+        { id: 1, tags: ['a', 'b'] },
+        { id: 2, tags: ['c', 'd'] },
+      ],
+      Test2: [
+        { id: 1, username: 'test' },
+        { id: 2, username: 'test2' },
+      ],
+    }
+
+    await importDatabase(data)
+
+    const tests = await Test.all()
+    const tests2 = await Test2.all()
+
+    assert.deepEqual(tests, data.Test)
+    assert.deepEqual(tests2, data.Test2)
+  })
+})
